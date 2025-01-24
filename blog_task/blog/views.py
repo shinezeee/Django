906d26f42c2db58bb1a554e4983aed688f857021
shinezeee <h46998600@gmail.com>
2025-01-24 13:36:16 +0900

    ✨ 수정 및 삭제 ,페이지네이션

--- blog_task/blog/views.py
+++ blog_task/blog/views.py
@@ -1,4 +1,7 @@
+from django.contrib.admin.templatetags.admin_list import pagination
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
+from django.db.models import Q
 
 from blog.forms import BlogForm
 from blog.models import Blog
@@ -8,10 +11,24 @@ from django.urls import reverse
 
 def blog_list(request):
     blogs = Blog.objects.all().order_by('-created_at') # 최신순으로
+
+    #검색대상 설정
+    q = request.GET.get('q')
+    if q:
+        blogs = blogs.filter(
+            Q(title__icontains=q) |
+            Q(content__icontains=q)
+        )
     visits = int(request.COOKIES.get('visits', 0)) + 1
     request.session['count'] =request.session.get('count', 0) + 1
-
-    context = {"blogs": blogs}
+    # 한 페이지당 10개씩 표시
+    paginator = Paginator(blogs, 10)
+    page = request.GET.get('page') # => 쿼리 스트링 가져옴
+    page_object = paginator.get_page(page)
+    context = {
+        #"blogs": blogs,
+        'page_object': page_object,
+    }
     response = render(request, 'blog_list.html', context)
     response.set_cookie('visits', visits)
     return response
@@ -33,3 +50,19 @@ def blog_create(request):
         return redirect(reverse('blog_detail', kwargs={'pk': blog.pk}))
     context = {'form':form}
     return render(request, 'blog_create.html', context)
+
+@login_required()
+def blog_update(request, pk):
+    blog = get_object_or_404(Blog, pk=pk,author=request.user)
+    form = BlogForm(request.POST or None, instance=blog) # 기초 데이터
+    if form.is_valid():
+        blog=form.save()
+        return redirect(reverse('blog_detail', kwargs={'pk': blog.pk}))
+    context = {'blog': blog, 'form': form}
+    return render(request,'blog_update.html', context)
+
+@login_required()
+def blog_delete(request, pk):
+    blog = get_object_or_404(Blog, pk=pk,author=request.user)
+    blog.delete()
+    return redirect(reverse('blog_list'))
