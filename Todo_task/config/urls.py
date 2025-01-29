464a70bebf2ac8b19dad6214c9a3df6100696b0e
shinezeee <h46998600@gmail.com>
2025-01-30 00:16:02 +0900

    🐛 Chapter04
    chapter05 를 위해 띄어쓰기 및 코드 버그, 오타 수정 커밋

--- Todo_task/config/urls.py
+++ Todo_task/config/urls.py
@@ -17,11 +17,13 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 from todo.views import todo_list, todo_info, todo_create, todo_update, todo_delete
 from users import views as user_views
 
 urlpatterns = [
+    # FBV
     path("todo/", todo_list, name="todo_list"),
     path("todo/create/",todo_create, name="todo_create"),
     path("todo/<int:todo_id>/",todo_info, name="todo_info"),
@@ -30,7 +32,6 @@ urlpatterns = [
 
     path("admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
-    # 유저관련
     path("signup/", user_views.signup, name="signup"),
     path("login/", user_views.login, name="login"),
 
