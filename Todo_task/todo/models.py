abc8eab977284dc6edf97146935c31c300fb2eda
shinezeee <h46998600@gmail.com>
2025-02-03 19:33:07 +0900

    models.py 수정

--- Todo_task/todo/models.py
+++ Todo_task/todo/models.py
@@ -1,7 +1,10 @@
+
 from django.contrib.auth import get_user_model
 #from django.contrib.auth.models import User
 from django.db import models
-
+from PIL import Image
+from pathlib import Path
+from io import BytesIO
 # Create your models here.
 
 User = get_user_model()
@@ -13,6 +16,9 @@ class Todo(models.Model) :
     start_date = models.DateField() # 시작 날짜
     end_date = models.DateField() # 끝나는 날짜
     is_done = models.BooleanField(default=False) # 완료 여부
+    thumbnail = models.ImageField(
+        upload_to = 'todo/thumbnails', default='todo/no_image/NO-IMAGE.gif',null=True, blank=True)
+    completed_image = models.ImageField(upload_to='todo/completed_images', null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True) # 생성 날짜
     updated_at = models.DateTimeField(auto_now=True) # 수정 날짜
 
@@ -20,6 +26,36 @@ class Todo(models.Model) :
     def __str__(self):
         return self.title
 
+    def save(self, *args, **kwargs):
+        if not self.completed_image:
+            return super().save(*args, **kwargs)
+
+        image = Image.open(self.completed_image)
+        image.thumbnail((100, 100))
+
+        image_path = Path(self.completed_image.name)
+
+        thumbnail_name = image_path.stem
+        thumbnail_extension = image_path.suffix
+        thumbnail_filename = f'{thumbnail_name}_thumbnail{thumbnail_extension}'
+
+        if thumbnail_extension in ['.jpg', '.jpeg']:
+            file_type = 'JPEG'
+        elif thumbnail_extension == '.png':
+            file_type = 'PNG'
+        elif thumbnail_extension == '.gif':
+            file_type = 'GIF'
+        else:
+            return super().save(*args, **kwargs)
+
+        temp_thumb = BytesIO()
+        image.save(temp_thumb, format=file_type)
+        temp_thumb.seek(0)
+
+        self.thumbnail.save(thumbnail_filename, temp_thumb, save=False)
+
+        temp_thumb.close()
+        return super().save(*args, **kwargs)
 
 class Comment(models.Model) :
     user = models.ForeignKey(User, on_delete=models.CASCADE)
