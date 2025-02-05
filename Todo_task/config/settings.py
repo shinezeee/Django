64907883059749c2e4ece836eb1bb8e6018eebe6
shinezeee <h46998600@gmail.com>
2025-02-05 11:40:57 +0900

    환경변수 설정(secret.json)

--- Todo_task/config/settings.py
+++ Todo_task/config/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import json
+import secrets
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-ijgd-^w$cd_sq&s@bbfg$3bf5#f433=6i%*bnebbf+*8-%p&z1"
+with open(BASE_DIR / 'secrets.json', 'r') as f:
+    secrets = json.load(f)
+SECRET_KEY = secrets["SECRET_KEY"]
+DEBUG = bool(secrets["DEBUG"])
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
